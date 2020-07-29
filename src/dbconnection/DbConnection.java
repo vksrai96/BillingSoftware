16e7fa482fe92f0f13e5f436ed544599527241f0
vksrai96 <47235229+vksrai96@users.noreply.github.com>
2020-07-29 04:11:11 +0200

    Add files via upload

---
+++ src/dbconnection/DbConnection.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dbconnection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Deepak
+ */
+public class DbConnection
+{
+    static Connection con;
+    public static Connection getConnect()
+    {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            con=DriverManager.getConnection("jdbc:mysql:///billing_software","root","");
+        }
+        catch(Exception e)
+        {
+            JOptionPane.showMessageDialog(null, "Error : "+e);
+        }
+        return con;
+    }
+}
---
+++ src/model/EmpDetails.java
@@ -0,0 +1,47 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author Deepak
+ */
+public class EmpDetails
+{
+
+    public EmpDetails() {
+    }
+    
+    private String name;
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhno() {
+        return phno;
+    }
+
+    public void setPhno(String phno) {
+        this.phno = phno;
+    }
+    private String phno;
+    
+    
+}
